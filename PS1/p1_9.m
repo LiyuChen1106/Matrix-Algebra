2c8cfc683ab2afd1ae1c96f3ea89243f148ba8da
Liyu Chen <liyuchen@Liyus-MacBook.local>
2018-09-22 22:57:08 -0400

    p1.9 finished

--- PS1/p1_9.m
+++ PS1/p1_9.m
@@ -2,46 +2,57 @@ close all; clc;
 
 [x,y]=meshgrid(-2:0.2:3.5, -2:0.2:3.5);
 f1 = 2*x+3*y+1;
-f2 = x.^2+y.^2-x*y-5;
+f2 = x.^2+y.^2-x.*y-5;
 f3 = (x-5)*cos(y-5)-(y-5)*sin(x-5);
 
-%figure(1)
-%plot_contour(x,y,f1,-10);
+figure(1)
+plotContour(x,y,f1,1);
+plotContour(x,y,f2,2);
+plotContour(x,y,f3,3);
 
-%figure(2)
-%plot_contour(x,y,f2);
+figure(2)
+plotTangentPlane(x,y,f1);
+figure(3)
+plotTangentPlane(x,y,f2);
+figure(4)
+plotTangentPlane(x,y,f3);
 
-%figure(1)
-%plot_contour(x,y,f1,-10);
+function plotContour(x,y,f,subPlotInd)
+subplot(1,3,subPlotInd);
 
-
-%contour3(x,y,f1)
-%grad_vec1 = subs(grad_f1,[x,y],[0,1]);
-[~,h] = contour(x,y,f2,25);
-h.ContourZLevel=-10;
+contour(x,y,f,21);
 hold on
 
-[dx,dy] = gradient(f2,.2,.2);
+[dx,dy] = gradient(f,.2,.2);
 x0 = 1;
 y0 = 0;
 t = (x == x0) & (y == y0);
 indt = find(t);
-f_grad2 = [dx(indt) dy(indt)];
-gradX = linespace(1,2);
-tangentX = linespace(-1,1);
+%f_grad2 = [dx(indt) dy(indt)];
+gradX = linspace(1,1.5);
+tangentX = linspace(0.5,1.5);
 
-gradY = dy(indt)/dx(indt)*gradX 
-%plot(xx,ylim,'b--o');
+gradY = dy(indt)/dx(indt)*(gradX - 1);
+tangentY = -dx(indt)/dy(indt)*(tangentX - 1);
+plot(gradX,gradY);
+hold on
+plot(tangentX,tangentY);
+axis equal
+xlabel('x');
+ylabel('y');
 hold off;
-%plot_contour(x,y,f1);
+end
 
-%figure(2)
-%surfc(x,y,f2)
-%contour3(x,y,f2)
-%grad_f2 = gradient(f2,[x,y]);
-%grad_vec2 = subs(grad_f2,[x,y],[0,1]);
-%plot3(x,y,grad_vec2);
+function plotTangentPlane(x,y,f)
+surf(x,y,f)
+hold on
 
-%figure(3)
-%surfc(x,y,f3)
-%contour3(x,y,f3)
+[dx,dy] = gradient(f,.2,.2);
+x0 = 1;
+y0 = 0;
+t = (x == x0) & (y == y0);
+indt = find(t);
+f_grad = [dx(indt) dy(indt)];
+f_plane=f(indt)+f_grad(1)*(x-1)+f_grad(2)*y;
+surf(x,y,f_plane);
+end
