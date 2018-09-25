e7b00db72e940b5bb7b3dce7d8371210c8abf885
Liyu Chen <liyuchen@Liyus-MacBook.local>
2018-09-24 23:21:04 -0400

    p1_13(b)

--- PS1/p1_13.m
+++ PS1/p1_13.m
@@ -7,7 +7,7 @@ syms x a0 a1 a2 a3 a4 a5;
 %a1=0.987862;
 V=[x.^0;x.^1;x.^2;x.^3;x.^4;x.^5];
 U=sin(x);%a0+a1*x+a2*x.^2+a3*x.^3+a4*x.^4+a5*x.^5;
-A=[a0;a1;a2;a3;a4;a5];
+%A=[a0;a1;a2;a3;a4;a5];
 
 %fun = @(x) V(x).*Q(x-1);
 %Q(1)=@(x) x.^(1-1)
@@ -18,7 +18,7 @@ Q=V;
 for i=1:6
     if(i>1)
         for j=1:i-1
-            innerprod = innerprod_(V(i),Q(j));%int(V(i)*Q(j),-pi,pi);
+            innerprod = innerprod_(Q(j),V(i));%int(V(i)*Q(j),-pi,pi);
             Q(i,1)=Q(i,1)-innerprod*Q(j);
         end
     end
@@ -28,17 +28,12 @@ end
 %Q
 %Q_norm
 
-bb=innerprod_(sin(x),Q(1))/innerprod_(Q(1),Q(1))
-
 %(b)
-E=Q;
+A=zeros(6,1);
 for i=1:6
-    proj=innerprod_(U,Q(i))*Q(i);
-    %proj
-    %func=(U-Q(i))*Q_norm(i);
-    %proj=dot(U,Q(i))*Q(i);
-    E(i)=int((U-proj)*Q(i),-pi,pi)==0;
+    A(i,1)=innerprod_(U,Q(i))/innerprod_(Q(i),Q(i));
 end
+
 
 function e = innerprod_(v_,q_)
 e=int(v_*q_,-pi,pi);
