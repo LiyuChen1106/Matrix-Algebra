8dca1210ffe59a0c78c8ef7ace79061c9d067276
Liyu Chen <liyuchen@Liyus-MacBook.local>
2018-09-24 23:50:26 -0400

    Finished 1-13

--- PS1/p1_13.m
+++ PS1/p1_13.m
@@ -1,17 +1,9 @@
 
-
-
 %Gram-Schmidt
 %x=linspace(-pi,pi);
-syms x a0 a1 a2 a3 a4 a5;
-%a1=0.987862;
+syms x;
 V=[x.^0;x.^1;x.^2;x.^3;x.^4;x.^5];
-U=sin(x);%a0+a1*x+a2*x.^2+a3*x.^3+a4*x.^4+a5*x.^5;
-%A=[a0;a1;a2;a3;a4;a5];
-
-%fun = @(x) V(x).*Q(x-1);
-%Q(1)=@(x) x.^(1-1)
-
+U=sin(x);
 
 %(a)
 Q=V;
@@ -25,8 +17,7 @@ for i=1:6
     norm=sqrt(innerprod_(Q(i),Q(i)));
     Q(i)=Q(i)/norm;
 end
-%Q
-%Q_norm
+
 
 %(b)
 A=zeros(6,1);
@@ -34,12 +25,23 @@ for i=1:6
     A(i,1)=innerprod_(U,Q(i))/innerprod_(Q(i),Q(i));
 end
 
+f=0;
+for i=1:6
+   f= f+A(i)*Q(i);
+end
+
+hold on 
+fplot(f,[-pi,pi])
+
+hold on
+fplot(U,[-pi,pi])
+
+hold on 
+fplot(x-x^3/6+x^5/120, [-pi,pi])
+
+legend('Orthonomal basis approximation','sinx','Taylor approximation')
 
 function e = innerprod_(v_,q_)
 e=int(v_*q_,-pi,pi);
 end
-%{E
-%[G,H]= equationsToMatrix(E, [a0,a1,a2,a3,a4,a5]);
-%G
-%H
-%[A,R]= linsolve(G,H)
+
